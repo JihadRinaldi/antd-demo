c82def58c09cba4377517d1ed69fb5de7d111f89
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-16 14:51:08 +0700

    add form antd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.min.css';
 import './App.css';
+import { Button, Input, Form } from 'antd';
 
 function App() {
+  const onFinish = (e) => {
+    console.log(e);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form onFinish={onFinish}>
+          <Form.Item
+            label="Username"
+            name="username"
+          >
+            <Input
+              placeholder="Username"
+              maxLength={30}
+              allowClear
+              required
+            />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+          >
+            <Input.Password
+              placeholder="Password"
+              maxLength={30}
+              allowClear
+              required
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              block
+              htmlType="submit"
+            >
+              Log In
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
---
+++ src/constants/index.constant.js
@@ -0,0 +1,1 @@
+export const FRUITS = ['Banana', 'Mango', 'Orange', 'Apple', 'Grape', 'Cherry'];
