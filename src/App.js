c0c66f1a71a93df4b8bfd03f766b92f5cee596aa
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-16 15:16:00 +0700

    add template for antd

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
-import { Button, Input, Form } from 'antd';
 
 function App() {
-  const onFinish = (e) => {
-    console.log(e);
-  };
   return (
     <div className="App">
-      <header className="App-header">
-        <Form onFinish={onFinish}>
-          <Form.Item
-            label="Username"
-            name="username"
-          >
-            <Input
-              placeholder="Username"
-              maxLength={30}
-              allowClear
-              required
-            />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-          >
-            <Input.Password
-              placeholder="Password"
-              maxLength={30}
-              allowClear
-              required
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              block
-              htmlType="submit"
-            >
-              Log In
-            </Button>
-          </Form.Item>
-        </Form>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
