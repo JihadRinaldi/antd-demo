0578fa21f3f13f4d16d1a1db5e2ba1b344e40f6c
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 11:22:10 +0700

    antd progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { Progress } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Progress
+          percent={45}
+          type="line"
+        />
+        <Progress
+          percent={45}
+          type="line"
+          strokeColor="red"
+        />
+        <Progress
+          percent={45}
+          type="circle"
+          status="normal"
+        />
+        <Progress
+          percent={45}
+          type="circle"
+          status="success"
+        />
+        <Progress
+          percent={45}
+          type="circle"
+          status="exception"
+        />
+        <Progress
+          percent={45}
+          type="line"
+          status="success"
+        />
+        <Progress
+          percent={45}
+          type="line"
+          status="success"
+          steps={3}
+        />
+      </header>
     </div>
   );
 }
