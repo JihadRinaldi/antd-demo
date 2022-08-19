3051ed01cd4988f1e86c60ecd1c7c5ef061720cb
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 10:55:09 +0700

    table antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { Table } from 'antd';
+import { TABLE_COLUMN, generateTableData } from './constants/index.constant';
 
 function App() {
+  const tableData = generateTableData();
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Table
+          dataSource={tableData}
+          columns={TABLE_COLUMN}
+          pagination
+          sticky
+        ></Table>
+      </header>
     </div>
   );
 }
--- src/constants/index.constant.js
+++ src/constants/index.constant.js
@@ -1,1 +1,55 @@
 export const FRUITS = ['Banana', 'Mango', 'Orange', 'Apple', 'Grape', 'Cherry'];
+
+export function generateTableData() {
+  const maxAge = 40;
+  const minAge = 17;
+
+  const TABLE_DATA = [];
+  for (let i = 0; i < 100; i++) {
+    TABLE_DATA.push({
+      key: i,
+      name: `User ${i}`,
+      age: Math.floor(Math.random() * (maxAge - minAge + 1)) + minAge,
+      address: `Jakarta Park no. ${i}`,
+    });
+  }
+  return TABLE_DATA;
+}
+
+export const TABLE_COLUMN = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'key',
+    fixed: 'left',
+    width: 150,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'key',
+    sorter: (a, b) => a.age - b.age,
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'key',
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'key',
+    width: 150,
+  },
+
+  {
+    title: 'Graduated',
+    key: 'key',
+    render: (payload) => {
+      return <p>{payload.age > 20 ? 'Yes' : 'No'}</p>;
+    },
+    width: 150,
+  },
+];
