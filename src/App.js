84143c6a16fee97229c8895c0f0033abbd26d2c1
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 14:56:48 +0700

    antd-table-customSelection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { Table } from 'antd';
+import { TABLE_COLUMN, TABLE_DATA } from './constants/index.constant';
+import { useState } from 'react';
 
 function App() {
+  const [selectedRow, setSelectedRow] = useState([]);
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Table
+          columns={TABLE_COLUMN}
+          dataSource={TABLE_DATA}
+          rowKey="id"
+          rowSelection={{
+            type: 'checkbox',
+            selectedRowKeys: selectedRow,
+            onChange: (keys) => {
+              setSelectedRow(keys);
+            },
+            onSelect: (record) => {
+              console.log(record);
+            },
+            getCheckboxProps: (record) => ({}),
+            selections: [
+              Table.SELECTION_NONE,
+              Table.SELECTION_ALL,
+              Table.SELECTION_INVERT,
+              {
+                key: 'even',
+                text: 'Select Even Rows',
+                onSelect: (allKeys) => {
+                  const selectedKeys = allKeys.filter((key) => {
+                    return key % 2 === 0;
+                  });
+                  setSelectedRow(selectedKeys);
+                },
+              },
+              {
+                key: 'excellent',
+                text: 'Select Student with Excellent Grads',
+                onSelect: (allkeys) => {
+                  const selectedKeys = allkeys.filter((key) => {
+                    const isExcellent = TABLE_DATA.find((student) => {
+                      return student.key === key && student.grade.includes('A');
+                    });
+                    return isExcellent;
+                  });
+                  setSelectedRow(selectedKeys);
+                },
+              },
+            ],
+          }}
+        />
+      </header>
     </div>
   );
 }
--- src/constants/index.constant.js
+++ src/constants/index.constant.js
@@ -1,1 +1,69 @@
+import { Tag } from 'antd';
+
 export const FRUITS = ['Banana', 'Mango', 'Orange', 'Apple', 'Grape', 'Cherry'];
+
+export const TABLE_COLUMN = [
+  {
+    title: 'Student ID',
+    dataIndex: 'id',
+  },
+  {
+    title: 'Student Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Student Grade',
+    dataIndex: 'grade',
+    render: (tag) => {
+      let color;
+      if (tag.includes('A')) {
+        color = 'green';
+      } else if (tag.includes('B')) {
+        color = 'blue';
+      } else {
+        color = 'red';
+      }
+      return (
+        <Tag
+          key={tag}
+          color={color}
+        >
+          {tag}
+        </Tag>
+      );
+    },
+  },
+];
+
+export const TABLE_DATA = [
+  {
+    key: 1,
+    id: 1,
+    name: 'Student Name 1',
+    grade: 'A+',
+  },
+  {
+    key: 2,
+    id: 2,
+    name: 'Student Name 2',
+    grade: 'A',
+  },
+  {
+    key: 3,
+    id: 3,
+    name: 'Student Name 3',
+    grade: 'B',
+  },
+  {
+    key: 4,
+    id: 4,
+    name: 'Student Name 4',
+    grade: 'C',
+  },
+  {
+    key: 5,
+    id: 5,
+    name: 'Student Name 5',
+    grade: 'A',
+  },
+];
