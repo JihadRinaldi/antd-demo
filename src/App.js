f845d981686eb6c9d6be29870e296ff3ef962f7f
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 11:05:38 +0700

    antd datepicker, rangepicker, and timepicker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { DatePicker } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <DatePicker picker="quarter" />
+        <DatePicker.RangePicker picker="month" />
+        <DatePicker.TimePicker />
+      </header>
     </div>
   );
 }
