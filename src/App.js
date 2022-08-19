89cf980ba9977471e2ab8f5ca514ae5c4d111b6b
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 14:05:48 +0700

    add table with fetch data and filter and sort column

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { TABLE_COLUMN } from './constants/index.constant';
+import { useEffect, useState } from 'react';
+import { Table } from 'antd';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchDataTable = async () => {
+      try {
+        setLoading(true);
+        const data = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const dataToJson = await data.json();
+        setTableData(dataToJson);
+      } catch (Err) {
+        throw new Error('Something error');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataTable();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Table
+          loading={loading}
+          columns={TABLE_COLUMN}
+          dataSource={tableData}
+          rowKey="id"
+          pagination={{}}
+        />
+      </header>
     </div>
   );
 }
--- src/constants/index.constant.js
+++ src/constants/index.constant.js
@@ -1,1 +1,31 @@
 export const FRUITS = ['Banana', 'Mango', 'Orange', 'Apple', 'Grape', 'Cherry'];
+export const TABLE_COLUMN = [
+  {
+    key: '1',
+    title: 'ID',
+    dataIndex: 'id',
+  },
+  {
+    key: '2',
+    title: 'User ID',
+    dataIndex: 'userId',
+    sorter: (record1, record2) => {
+      return record1.userId > record2.userId;
+    },
+  },
+  {
+    key: '3',
+    title: 'Status',
+    dataIndex: 'completed',
+    render: (completed) => {
+      return <p>{completed ? 'Completed' : 'In Progress'}</p>;
+    },
+    filters: [
+      { text: 'Completed', value: true },
+      { text: 'In Progress', value: false },
+    ],
+    onFilter: (value, record) => {
+      return record.completed === value;
+    },
+  },
+];
