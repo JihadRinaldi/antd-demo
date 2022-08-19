4bc881a43b02ebbf3be32affd219aedf114f62df
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 11:01:46 +0700

    antd alert form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
-import { Button, Input, Form } from 'antd';
+import { Button, Input, Form, message, Alert } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
   const onFinish = (e) => {
     console.log(e);
+    setTimeout(() => {
+      message.error('Login Error');
+      setShowAlert(true);
+    }, 2000);
   };
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          {showAlert && (
+            <Alert
+              type="error"
+              description="There was an Error on login"
+              closable
+            />
+          )}
+        </div>
         <Form onFinish={onFinish}>
           <Form.Item
             label="Username"
