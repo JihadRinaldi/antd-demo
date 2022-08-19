d4ddfac6dc8d7f0d3f61578d79a509d1a0f0b3f1
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-19 11:11:31 +0700

    antd spinner loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { Spin, Button } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Spin spinning={loading} />
+        <Button
+          onClick={() => {
+            setLoading((prevState) => !prevState);
+          }}
+        >
+          Trigger Loading
+        </Button>
+        <Spin spinning={loading}>
+          {loading ? 'Fetching Data' : 'Data1 Data2 Data3'}
+        </Spin>
+      </header>
     </div>
   );
 }
