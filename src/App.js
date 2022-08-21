5d6618a5e511ee78d54a9a4d6538f130f692d914
Jihad Rinaldi <jihadmjr@Jihads-MacBook-Pro.local>
2022-08-21 14:32:47 +0700

    antd table add, update, delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
 import 'antd/dist/antd.min.css';
 import './App.css';
+import { useState } from 'react';
+import { TABLE_DATA } from './constants/index.constant';
+import { Table, Modal } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 function App() {
+  const [dataSource, setDataSource] = useState(TABLE_DATA);
+  const TABLE_COLUMN = [
+    {
+      key: '1',
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      key: '2',
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      key: '3',
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      key: '4',
+      title: 'Address',
+      dataIndex: 'address',
+    },
+    {
+      key: '5',
+      title: 'Actions',
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined />
+            <DeleteOutlined
+              onClick={() => onDeleteStudent(record)}
+              style={{ color: 'red', marginLeft: 12 }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const onAddStudent = () => {
+    const randomNumber = Math.random() * 1000;
+    const newStudent = {
+      id: randomNumber,
+      name: `Name ${randomNumber}`,
+      email: `${randomNumber} @gmail.com`,
+      address: `Address ${randomNumber}`,
+    };
+    setDataSource((prevData) => {
+      return [...prevData, newStudent];
+    });
+  };
+
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: 'Are you sure want to delete this student record?',
+      onOk: () => {
+        setDataSource((prevData) => {
+          return prevData.filter((student) => student.id !== record.id);
+        });
+      },
+      okType: 'danger',
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Table
+          columns={TABLE_COLUMN}
+          dataSource={dataSource}
+          rowKey="id"
+        ></Table>
+      </header>
     </div>
   );
 }
--- src/constants/index.constant.js
+++ src/constants/index.constant.js
@@ -1,1 +1,28 @@
 export const FRUITS = ['Banana', 'Mango', 'Orange', 'Apple', 'Grape', 'Cherry'];
+
+export const TABLE_DATA = [
+  {
+    id: 1,
+    name: 'John',
+    email: 'John@gmail.com',
+    address: 'John Address',
+  },
+  {
+    id: 2,
+    name: 'David',
+    email: 'David@gmail.com',
+    address: 'David Address',
+  },
+  {
+    id: 3,
+    name: 'Sam',
+    email: 'Sam@gmail.com',
+    address: 'Sam Address',
+  },
+  {
+    id: 4,
+    name: 'James',
+    email: 'James@gmail.com',
+    address: 'James Address',
+  },
+];
